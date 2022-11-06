dfaade61faef357793d800c90b20c68dc0d2c3d8
ductambka <ductambka@gmail.com>
2022-11-06 10:33:35 +0700

    Fix tests...

--- tests/test_tnd_translator.py
+++ tests/test_tnd_translator.py
@@ -4,7 +4,17 @@ from tnd_translator.translator import translate
 
 
 class TestTndTranslator(unittest.TestCase):
-    def test_translate_en_fr(self):
+    def test_frenchToEnglish(self):
+        a = translate("bonjour", from_lang_code2="en", to_lang_code2="fr")
+        trans_text = ""
+        if len(a) > 0:
+            trans_text = a[0]['translation'].lower()
+
+        self.assertEqual(trans_text, "hello")
+        self.assertNotEqual(trans_text, "hello1")
+
+
+    def test_englishToFrench(self):
         a = translate("Hello", from_lang_code2="en", to_lang_code2="fr")
         trans_text = ""
         if len(a) > 0:
